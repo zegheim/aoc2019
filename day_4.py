c382301f103631d62dda231fd7c0f226eecac06f
zegheim <justinarista@gmail.com>
2019-12-13 18:56:04 +0000

    added template

---
+++ day_4.py
@@ -0,0 +1,10 @@
+def main():
+    print("Part 1\n------")
+    print("Answer for part 1: {}\n".format(None))
+
+    print("Part 2\n------")
+    print("Answer for part 2: {}\n".format(None))
+
+
+if __name__ == "__main__":
+    main()
